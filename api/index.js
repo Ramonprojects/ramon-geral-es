5374a4432f2edc047e2ec5ecdd2a1fcc348a0640
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-23 15:10:19 -0300

    chore: update shareCode to B2LFGZFARZR

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=8A4NHR5K905&afp=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=B2LFGZFARZR&afp=ot' });
     res.end();
   }
