1c3aeb91a9a1db8e05e78cc89d5e1122bdc3ab84
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-26 10:17:39 -0300

    chore: update shareCode to KBJB93M5WJE

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=WKMQVWZNFTZ&afp=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=KBJB93M5WJE&afp=ot' });
     res.end();
   }
