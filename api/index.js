27812460142e748774c1515ede2b16f4a18bceaf
Ramonprojects <adm@ramonmago.com>
2026-06-01 14:48:29 -0300

    chore: shareCode TZD17L5DBVQ -> W5MXFZ6QEAR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=TZD17L5DBVQ&afp=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=W5MXFZ6QEAR&afp=ot' });
     res.end();
   }
