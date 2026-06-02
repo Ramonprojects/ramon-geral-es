417ddee3aeecfb11070ca0179eada8033bee8b03
Ramonprojects <adm@ramonmago.com>
2026-06-02 09:46:54 -0300

    chore: shareCode W5MXFZ6QEAR -> SQQJUIDPN4B (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=W5MXFZ6QEAR&afp=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=SQQJUIDPN4B&afp=ot' });
     res.end();
   }
