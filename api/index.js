67ec4b6f28a54eb962593dc8da02c941c773080e
Ramonprojects <adm@ramonmago.com>
2026-06-06 17:29:02 -0300

    chore: shareCode WU1N20SYXCM -> AUGITUO22BC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=WU1N20SYXCM&afp=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=AUGITUO22BC&afp=ot' });
     res.end();
   }
