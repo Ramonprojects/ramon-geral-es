5a4bf7dc775d49ba2180c4d5369f0388d4b0dc6e
Ramonprojects <adm@ramonmago.com>
2026-06-07 14:11:41 -0300

    chore: shareCode AUGITUO22BC -> TL2YVZSOT3M (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=AUGITUO22BC&afp=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=TL2YVZSOT3M&afp=ot' });
     res.end();
   }
