2e9dad336583166d03811880ad866bd99278c77d
Ramonprojects <adm@ramonmago.com>
2026-06-14 13:55:03 -0300

    chore: shareCode YQ3OM8QX90P -> TMDQEZF1LKY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=YQ3OM8QX90P&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=TMDQEZF1LKY&afp1=ot' });
     res.end();
   }
