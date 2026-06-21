0a04bc8388f7cd247ebc56f7c6ad7d4e1afa9771
Ramonprojects <adm@ramonmago.com>
2026-06-21 12:16:31 -0300

    chore: shareCode F98V12PZ32P -> A1RQ3ZJ9523 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=F98V12PZ32P&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=A1RQ3ZJ9523&afp1=ot' });
     res.end();
   }
