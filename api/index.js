e2a6948d7cf0d5d68100a62770e24b273b8cef16
Ramonprojects <adm@ramonmago.com>
2026-06-28 12:38:45 -0300

    chore: shareCode 6W1S4NMKL4T -> KCB00ZHKRFI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=6W1S4NMKL4T&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=KCB00ZHKRFI&afp1=ot' });
     res.end();
   }
