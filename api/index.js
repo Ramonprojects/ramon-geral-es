35f69c837e44f1e4e034c5c438502828b8bfca50
Ramonprojects <adm@ramonmago.com>
2026-06-29 12:55:25 -0300

    chore: shareCode KCC13GZFPD6 -> V72HA64NHU1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=KCC13GZFPD6&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=V72HA64NHU1&afp1=ot' });
     res.end();
   }
