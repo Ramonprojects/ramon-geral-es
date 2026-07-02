80ccfd3b4e80d80b26e23bec189f783b389f5243
Ramonprojects <adm@ramonmago.com>
2026-07-02 09:42:37 -0300

    chore: shareCode FTRSJNP2NHJ -> U4O1LAHRDDO (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=FTRSJNP2NHJ&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=U4O1LAHRDDO&afp1=ot' });
     res.end();
   }
