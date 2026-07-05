1d81d79ed5d976cbdc96421285442bc4a8c213e1
Ramonprojects <adm@ramonmago.com>
2026-07-05 09:23:01 -0300

    chore: shareCode YK0OF7V54UB -> SOP9BFBW5GV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=YK0OF7V54UB&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=SOP9BFBW5GV&afp1=ot' });
     res.end();
   }
