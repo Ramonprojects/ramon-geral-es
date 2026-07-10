8dbf278d5fb4ebcd2b1b6b112c2b260be03437eb
Ramonprojects <adm@ramonmago.com>
2026-07-10 11:46:08 -0300

    chore: shareCode IRUW7TEI6IY -> 3NAXP1WVX4B (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=IRUW7TEI6IY&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=3NAXP1WVX4B&afp1=ot' });
     res.end();
   }
