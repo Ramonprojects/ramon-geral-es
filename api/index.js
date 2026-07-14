29c3f06425edb062d3a342ffe0db935167635b70
Ramonprojects <adm@ramonmago.com>
2026-07-14 12:50:20 -0300

    chore: shareCode Z75RNCPOINQ -> K9J8GER4ZDD (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=Z75RNCPOINQ&afp1=ot' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=K9J8GER4ZDD&afp1=ot' });
     res.end();
   }
