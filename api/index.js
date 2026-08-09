a13e9f662c32b9840bc391798d4f8618f8863181
Ramonprojects <adm@ramonmago.com>
2026-08-09 12:20:38 -0300

    chore: shareCode VOA6GTMNQ81 -> FP4J2NOVARJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=VOA6GTMNQ81&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=FP4J2NOVARJ&afp1=ou' });
     res.end();
   }
