54e2b8f7a7620d3aa976516acdd2c2c6174f47a2
Ramonprojects <adm@ramonmago.com>
2026-08-12 12:36:26 -0300

    chore: shareCode AHVFGP94PCF -> 5YM0C8OKDNH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=AHVFGP94PCF&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=5YM0C8OKDNH&afp1=ou' });
     res.end();
   }
