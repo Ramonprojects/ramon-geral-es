e70b9abc257fc50d6fa3c0451cdfcde5fcf54f18
Ramonprojects <adm@ramonmago.com>
2026-08-22 12:34:45 -0300

    chore: shareCode TKSIZ39MBPF -> FJR7XDO93VI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=TKSIZ39MBPF&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=FJR7XDO93VI&afp1=ou' });
     res.end();
   }
