de0f4d86e3580c92dd85e1eaefce6e126f7a533f
Ramonprojects <adm@ramonmago.com>
2026-08-24 15:52:10 -0300

    chore: shareCode V1A76918L7H -> 1FWHGQ4MF8W (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=V1A76918L7H&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=1FWHGQ4MF8W&afp1=ou' });
     res.end();
   }
