7217e7ba099d5966faa27f0ad349e605e29e2887
Ramonprojects <adm@ramonmago.com>
2026-09-03 14:25:35 -0300

    chore: shareCode KLN61WZD9I7 -> 61YXXJ1THCU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=KLN61WZD9I7&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=61YXXJ1THCU&afp1=ou' });
     res.end();
   }
