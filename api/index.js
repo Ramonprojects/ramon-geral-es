e548d792ff7ea483ffef1a2d343405215b211f9a
Ramonprojects <adm@ramonmago.com>
2026-09-06 13:15:25 -0300

    chore: shareCode 4KEMGIKVQA0 -> ZX3R55M354M (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=4KEMGIKVQA0&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=ZX3R55M354M&afp1=ou' });
     res.end();
   }
