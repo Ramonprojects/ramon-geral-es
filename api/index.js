b403b0eb5f4067843f39bbbf44b6874c34b99f2f
Ramonprojects <adm@ramonmago.com>
2026-09-16 14:48:59 -0300

    chore: shareCode RGOVHQUKIWA -> GCNGI5XFRQA (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=RGOVHQUKIWA&afp1=ou' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=GCNGI5XFRQA&afp1=ou' });
     res.end();
   }
